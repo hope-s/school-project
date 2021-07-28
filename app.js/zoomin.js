c9c1390cfa509611bf62fa8961e07ca86e229387
omidprogrammer <72966159+omidprogrammer@users.noreply.github.com>
2021-07-28 18:01:06 +0430

    update to version 8.4

---
+++ app.js/zoomin.js
@@ -0,0 +1,30 @@
+//function to zoom image
+function imageZoom(){
+    var $zoomimg = $('img.zoomimg');
+  
+    //$zoomimg.mouseover(function(){
+      //$(this).css({"cursor":"zoom-in","outline":"1px solid #fff"});
+    //});
+    //create overlay, load large data-src image	into src
+    $zoomimg.on('click',function(e){
+      e.stopPropagation();
+      $(this).after('<div id="imgZoomDiv"><div><img src="" class="zoomed" /></div></div>');
+      var $t = $(this);
+      $('.zoomed').attr({src: $t.attr('data-src')});
+      //$('#imgZoomDiv').animate({opacity:"1"}, 250).show();
+      $('#imgZoomDiv').show();
+  
+      $('#imgZoomDiv > div').hide().prepend('<i class="fa fa-times-circle close"></i>').fadeIn(2222);// add close button
+      setTimeout($.scrollLock(true),250);
+  
+    });
+    //remove overlay - imgZoomDiv
+    $(document).on('click', '#imgZoomDiv', function(e){
+      e.preventDefault();
+      //$('.zoomed').attr('src','');
+      //$(this).animate({opacity:"0"}, 250).hide().remove();
+      $(this).hide().remove();
+      setTimeout($.scrollLock(false),500);
+    });
+  };
+  imageZoom();
