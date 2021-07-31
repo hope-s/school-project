3225ea74f098bd5627002d72d2fcc9a49248b960
omidprogrammer <72966159+omidprogrammer@users.noreply.github.com>
2021-07-31 11:42:07 +0430

    fixed zoom in

--- app.js/zoomin.js
+++ app.js/zoomin.js
@@ -1,11 +1,6 @@
 //function to zoom image
 function imageZoom(){
     var $zoomimg = $('img.zoomimg');
-  
-    //$zoomimg.mouseover(function(){
-      //$(this).css({"cursor":"zoom-in","outline":"1px solid #fff"});
-    //});
-    //create overlay, load large data-src image	into src
     $zoomimg.on('click',function(e){
       e.stopPropagation();
       $(this).after('<div id="imgZoomDiv"><div><img src="" class="zoomed" /></div></div>');
@@ -15,16 +10,11 @@ function imageZoom(){
       $('#imgZoomDiv').show();
   
       $('#imgZoomDiv > div').hide().prepend('<i class="bi bi-x close"></i>').fadeIn(500);// add close button
-      setTimeout($.scrollLock(true),250);
   
     });
-    //remove overlay - imgZoomDiv
     $(document).on('click', '#imgZoomDiv', function(e){
       e.preventDefault();
-      //$('.zoomed').attr('src','');
-      //$(this).animate({opacity:"0"}, 250).hide().remove();
       $(this).hide().remove();
-      setTimeout($.scrollLock(false),500);
     });
   };
   imageZoom();
